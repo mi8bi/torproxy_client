5b7798bca8d640819096b2ffb2f6f33b5c6f9697
mi8bi <151357186+mi8bi@users.noreply.github.com>
2025-03-23 15:54:27 +0900

    version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torproxy_client',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     license='MIT',
     description='A simple Python library to connect to the internet through Tor proxy.',
